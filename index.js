cb475ae29c3ef48c6e73469b9d5cb76586ded9ff
gloria <gloria.gao115@gmail.com>
2023-02-28 13:05:02 +1100

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.querySelector('main').remove()
+
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = "victory"
+
+newHeader.innerHTML = "is the champion"
